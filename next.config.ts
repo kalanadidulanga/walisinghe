c7107ccbae79bedf7abee78b0ae3916c994c8826
Kalana Didulanga Koralegedara <kalanadidulanga2005@gmail.com>
2025-02-01 18:37:57 +0530

    asdasd

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  crossOrigin: 'anonymous',
+  reactStrictMode: true,
 };
 
 export default nextConfig;
